6650dffc184709d6d1eda458bc599b6a575fa2d0
haapseem <haapseem@ad.helsinki.fi>
2018-09-23 23:26:44 +0300

    vk 3 t9-10 kesken

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -6,6 +6,7 @@ class Beer < ApplicationRecord
 
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
+  has_many :raters, -> { distinct }, through: :ratings, source: :user
 
   validates :name, length: { minimum: 1 }
 
